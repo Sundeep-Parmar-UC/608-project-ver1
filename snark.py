d25417e5d50d9a3cc518adf2b8ae01d4473b1c73
Sundeep-Parmar-UC <sundeep.parmar@ucalgary.ca>
2026-03-30 22:33:15 -0600

    Add files via upload

--- snark.py
+++ snark.py
@@ -26,7 +26,7 @@ dict_SnarkLevel= {
 import google.generativeai as genai
 
 
-genai.configure(api_key="AIzaSyADTCTsKrUFjwNeD1DWjK9Ijgp9W7gGx3U")
+genai.configure(api_key="XXXXXXXXXXXX")
 model = genai.GenerativeModel("gemini-3.1-flash-lite-preview")
 
 # return remark text
